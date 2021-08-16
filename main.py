dc768061ba74116152c9e7181b72ba4048ee681b
shubham805 <shubham805@gmail.com>
2021-08-16 13:30:41 +0530

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from flask import Flask
+from structlog import get_logger
+
+app = Flask(__name__)
+
+log = get_logger()
+
+
+@app.route("/")
+def hello_world():
+    log.info("Info log")
+    log.debug("Debug log")
+    log.error("Error Log")
+    print("Console write")
+    return "<p>Hello, World!</p>"
