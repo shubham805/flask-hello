f8aedfdd49c63d85b957e988231252a0898791a7
Shubham Jain <shubham805@gmail.com>
2024-01-25 14:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 
 from flask import Flask
-from structlog import get_logger
 
 app = Flask(__name__)
 
@@ -14,10 +13,6 @@ log = get_logger()
 
 @app.route("/")
 def hello_world():
-    log.info("Info log")
-    log.debug("Debug log")
-    log.error("Error Log")
-    print("Console write")
     return "<p>Hello, World!</p>"
 
 app.run(host='0.0.0.0', port=5000)
